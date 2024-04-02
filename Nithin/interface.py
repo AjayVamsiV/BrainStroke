156590e5285f1d18946586e132aa297ae3d4b916
AjayVamsiV <116199429+AjayVamsiV@users.noreply.github.com>
2024-04-02 19:50:47 +0530

    Update interface.py

--- Nithin/interface.py
+++ Nithin/interface.py
@@ -14,15 +14,15 @@ def main():
 
     # Add dropdowns for each input feature
     gender = st.selectbox('Select Gender', ['Male', 'Female'])
+    age = st.number_input('Enter Age', format='%d', value=0)
     hypertension = st.selectbox('Select Hypertension', [0, 1])
     heart_disease = st.selectbox('Select Heart Disease', [1, 0])
     ever_married = st.selectbox('Select Ever Married', ['Yes', 'No'])
     work_type = st.selectbox('Select Work Type', ['Private', 'Self-employed', 'Govt_job', 'children'])
     Residence_type = st.selectbox('Select Residence Type', ['Urban', 'Rural'])
-    smoking_status = st.selectbox('Select Smoking Status', ['formerly smoked', 'never smoked', 'smokes', 'Unknown'])
-    age = st.number_input('Enter Age', format='%d', value=0)
     glucose_level = st.number_input('Enter Glucose Level', format='%f', value=0.0)
     bmi = st.number_input('Enter BMI', format='%f', value=0.0)
+    smoking_status = st.selectbox('Select Smoking Status', ['formerly smoked', 'never smoked', 'smokes', 'Unknown'])
     # Add a button to trigger the prediction
     if st.button('Predict'):
         preprocessedData = encode(gender, age, hypertension, heart_disease, ever_married, work_type, Residence_type, glucose_level, bmi, smoking_status)
